c4833cd2cdc4d0369f55900153109c731fee5fc6
tomas.kravcik <thomas.kravcik@gmail.com>
2026-03-07 16:47:58 +0100

    feat: Convert Astro project to static site generation (SSG) with `getStaticPaths` and add a GitHub Actions deployment workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomasastro.cz',
-  output: 'server',
+  output: 'static',
 
   i18n: {
     defaultLocale: 'cz',
@@ -18,10 +18,11 @@ export default defineConfig({
   },
 
   integrations: [react(), sitemap()],
-
-  adapter: node({
-    mode: 'standalone'
-  }),
+  /*
+    adapter: node({
+      mode: 'standalone'
+    }),
+  */
 
   vite: {
     plugins: [tailwindcss()]
